a51c657d1e823ab619145f85e9eb0a43eba0b649
OrginalS <36825453+OrginalS@users.noreply.github.com>
2019-11-18 16:16:44 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -9,11 +9,9 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # ship settings
-        self.ship_speed = 1.5
         self.ship_limit = 3
 
         # bullet settings
-        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
@@ -25,19 +23,37 @@ class Settings:
         # how quickly the game speeds up
         self.speedup_scale = 1.1
 
+        # how quickly the alien point values increase
+        self.score_scale = 1.5
+
+        # cpu limiter
+        self.cpu_speed = 0.1
+
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed = 1.5
-        self.bullet_speed = 3.0
-        self.alien_speed = 1.0
+        self.ship_speed = 1.5 * self.cpu_speed * 4
+        self.bullet_speed = 3.0 * self.cpu_speed * 4
+        self.alien_speed = 1.0 * self.cpu_speed
 
         # fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
 
+        # scoring
+        self.alien_points = 50
+
     def increase_speed(self):
-        """Increase speed settings."""
-        self.ship_speed *= self.speedup_scale
-        self.bullet_speed *= self.speedup_scale
+        """Increase speed settings and alien point values."""
+        #self.ship_speed *= self.speedup_scale
+        #self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+        self.alien_points = int(self.alien_points * self.score_scale)
+
+    def difficulty(self, diff):
+        if diff == 1:
+            self.speedup_scale = 1.1
+        elif diff == 2:
+            self.speedup_scale = 1.3
+        elif diff == 3:
+            self.speedup_scale = 1.5
